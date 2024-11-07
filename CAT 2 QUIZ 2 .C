92b1e1079dacd3e1679f98cabe47ac7fe2a01ce6
Evanohph <nkonge.21954@students.kyu.ac.ke>
2024-11-07 10:58:11 -0800

    Create CAT 2 QUIZ 2 .C

---
+++ CAT 2 QUIZ 2 .C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   
+    int scores[2][2] = {
+        {85, 90},
+        {75, 80}
+    };
+
+    
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("scores[%d][%d] = %d\n", i, j, scores[i][j]);
+        }
+    }
+
+    return 0;
+}
